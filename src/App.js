fc5fe783aab74dc2e276002011b99dd992f73cbe
ANSHUMAN-PC\Anshuman <anshumas@hotmail.com>
2018-09-18 00:55:45 -0500

    calling functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+state={
+  Persons:[
+    {name:"Anshuman1", age:28},
+    {name:"Anshuman4", age:29},
+    {name:"Anshuman5", age:27}
+  ]
+};
+changeNameHandler=()=>{
+
+  this.setState({
+    Persons:[
+      {name:"Anshuman11", age:48},
+      {name:"Anshuman41", age:49},
+      {name:"Anshuman51", age:47}
+    ] 
+  });
+};
+revertNameHandler=()=>{
+
+  this.setState({
+    Persons:[
+      {name:"Anshuman1", age:28},
+      {name:"Anshuman4", age:29},
+      {name:"Anshuman5", age:27}
+    ]
+  });
+};
+changeNameHandler=(event)=>{
+
+  this.setState({
+    Persons:[
+      {name:"Anshuman1", age:28},
+      {name:event.target.value, age:29},
+      {name:"Anshuman5", age:27}
+    ]
+  });
+};
   render() {
     return (
       <div className="App">
@@ -13,14 +50,28 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. 
    </p>
-        <Person name="anshuman1" age="13">d</Person>
-        <Person name="anshuman2" age="23">w</Person>
-        <Person name="anshuman3" age="33">q</Person>
+   <button onClick={this.changeNameHandler}>App update Name </button>
+   <button onClick={this.revertNameHandler}>App revert Name </button>
+        <Person 
+          name={this.state.Persons[0].name} 
+          // clickUpdate={this.changeNameHandler}  
+          // clickRevert={this.revertNameHandler}  
+          changeName={this.changeNameHandler}  
+          age={this.state.Persons[0].age}>
+        </Person>
+        <Person 
+          name={this.state.Persons[1].name} changeName={this.changeNameHandler}  
+          age={this.state.Persons[1].age}>
+        </Person>
+        <Person 
+          name={this.state.Persons[2].name} changeName={this.changeNameHandler}  
+          age={this.state.Persons[2].age}>
+          q</Person>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,11 @@ const Person = (props) => {
     return (
         <div>
             <p>I am {props.name} and my age is {props.age}.</p>
-            <p>{props.children}</p>
+            <input type="text" onChange={props.changeName} value={props.name} />
+
+            {/* <button onClick={props.clickUpdate}>Person update Name </button>
+            <button onClick={props.clickRevert}>Person revert Name </button> */}
+
         </div>
     );
 }
